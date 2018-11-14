051f81fe7254e212e617c10aaaea44e45104caaa
weichao18 <weichao18@vip.qq.com>
2018-11-14 22:28:25 +0800

    Crop Diseases

--- slim/train.sh
+++ slim/train.sh
@@ -2,7 +2,7 @@ DATASET_DIR=/media/zh/DATA/AgriculturalDisease20181023/tf_data
 TRAIN_DIR=/media/zh/DATA/AgriculturalDisease20181023/check_save/resnetv1_101_finetune
 CHECKPOINT_PATH=/media/zh/DATA/AgriculturalDisease/check/resnet_v1_101.ckpt
 
-python train_image_classifier1.py \
+python train_image_classifier.py \
     --train_dir=${TRAIN_DIR} \
     --dataset_name=AgriculturalDisease \
     --dataset_split_name=train \
@@ -10,6 +10,6 @@ python train_image_classifier1.py \
     --model_name=resnet_v1_101 \
     --checkpoint_path=${CHECKPOINT_PATH} \
     --learning_rate=0.002 \
-    #--checkpoint_exclude_scopes=vgg_16/fc8
+    --checkpoint_exclude_scopes=vgg_16/fc8
 
 
